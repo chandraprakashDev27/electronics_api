dcd1b9edd15a814c4a41ca441230d93ba21d49f2
Navin Yele <navin.yele@intapp.com>
2024-07-27 21:47:42 +0530

    database updated

--- app/db.js
+++ app/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 
 // MySQL connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // replace with your MySQL username
-    password: '', // replace with your MySQL password
-    database: 'electronics_03'
+    host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+    user: '4Briw6qhHbDVRai.root', // replace with your MySQL username
+    password: 'jOwh11OI7YQTCeVL', // replace with your MySQL password
+    database: 'electronics',
+    port:"4000"
  // replace with your database name
 });
 
