ac3f781c814d0ba3ba479ff9f8961d54fad144a7
Navin Yele <navin.yele@intapp.com>
2024-07-27 22:06:34 +0530

    Database added successfully...!

--- app/db.js
+++ app/db.js
@@ -1,13 +1,15 @@
 const mysql = require('mysql2');
-
+const fs = require("fs")
 // MySQL connection
 const db = mysql.createConnection({
     host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
     user: '4Briw6qhHbDVRai.root', // replace with your MySQL username
     password: 'jOwh11OI7YQTCeVL', // replace with your MySQL password
     database: 'electronics',
-    port:"4000"
- // replace with your database name
+    port:"4000",
+    ssl:{
+        CA:fs.readFileSync('./app/ca.pem')
+    }
 });
 
 // Uncomment if you want to check database connection
